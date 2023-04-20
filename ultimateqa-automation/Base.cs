b185708f5dd594c4b722de42c6e2a70081f19024
Marilou van Schalkwyk <oddballmeo@gmail.com>
2023-04-20 19:04:24 +1000

    made some changes to base class and created the HomePage class

---
+++ ultimateqa-automation/Base.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ultimateqa_automation
+{
+    internal class Base
+    {
+        protected IWebDriver Driver { get; set;  }
+        protected WebDriverWait Wait { get; set; }
+        public Base(IWebDriver driver) 
+        {
+            Driver = driver;
+            Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        public HomePage HomePage()
+        {
+            return new HomePage(Driver);
+        }
+    }
+}
---
+++ ultimateqa-automation/HomePageTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ultimateqa_automation
+{
+    [TestClass]
+    [TestCategory("HomePage")]
+    public class HomePageTests
+    {
+        [Description("Navigating to Automation Excercise page from the homepage")]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
